c1caa9d2a72b3a1a6ab795b36f358f90398d4cc5
Emil-Jarosz <emil.jarosz01@gmail.com>
2020-11-24 23:57:46 +0000

    Add byte_swap

---
+++ include/bigj/unicode/detail/byte_swap.hpp
@@ -0,0 +1,19 @@
+#include <bit>
+
+#include <cstdint>
+
+namespace bigj {
+namespace unicode {
+namespace detail {
+
+constexpr auto byte_swap(uint16_t x) noexcept -> uint16_t {
+    return std::rotl(x, 8);
+}
+
+constexpr auto byte_swap(uint32_t x) noexcept -> uint32_t {
+    return std::rotl((x & 0xFF00FF00) >> 8 | (x & 0x00FF00FF) << 8, 16);
+}
+
+} // namespace detail
+} // namespace unicode
+} // namespace bigj
