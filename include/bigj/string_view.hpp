60914fe07cb88da68d4fe8284791aa33e337687f
Emil-Jarosz <emil.jarosz01@gmail.com>
2020-12-13 11:01:21 +0000

    Add string_view header
    Contains type aliases for unicode string views.

---
+++ include/bigj/string_view.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "basic_string_view.hpp"
+#include "unicode/utf8.hpp"
+#include "unicode/utf16.hpp"
+#include "unicode/utf32.hpp"
+
+namespace bigj {
+
+using utf8_string_view = basic_string_view<unicode::utf8>;
+using utf16be_string_view = basic_string_view<unicode::utf16be>;
+using utf32be_string_view = basic_string_view<unicode::utf32be>;
+using utf16le_string_view = basic_string_view<unicode::utf16le>;
+using utf32le_string_view = basic_string_view<unicode::utf32le>;
+
+using string_view = utf8_string_view;
+
+} // namespace bigj
