ad2ee98b79bcbb7f392a045fc2f677f360000810
Emil-Jarosz <emil.bigj.jarosz@gmail.com>
2021-08-26 19:29:18 +0100

    Tidy up encoding.hpp

--- include/bigj/unicode/encoding.hpp
+++ include/bigj/unicode/encoding.hpp
@@ -9,24 +9,22 @@ namespace bigj {
 namespace unicode {
 
 template<typename T>
-concept encoding = requires { typename T::code_unit; } && requires (
-    code_point value,
-    const typename T::code_unit* input_it,
-    const typename T::code_unit* end_it,
-    typename T::code_unit* output_it
-) {
-    { T::encoded_size(value) } noexcept -> std::same_as<size_t>;
-
-    { T::encode(value, output_it) } noexcept -> std::same_as<decltype(output_it)>;
-
-    { T::decode(input_it) } noexcept -> std::same_as<code_point>;
-
-    { T::validate(input_it, end_it) } noexcept -> std::same_as<error_code>;
-
-    { T::next_code_point(input_it) } noexcept -> std::same_as<decltype(input_it)>;
-
-    { T::prev_code_point(input_it) } noexcept -> std::same_as<decltype(input_it)>;
-};
+concept encoding =
+    requires { typename T::code_unit; }
+    && std::integral<typename T::code_unit>
+    && requires (
+        code_point value,
+        const typename T::code_unit* input_it,
+        const typename T::code_unit* end_it,
+        typename T::code_unit* output_it
+    ) {
+        { T::encoded_size(value) } noexcept -> std::same_as<size_t>;
+        { T::encode(value, output_it) } noexcept -> std::same_as<decltype(output_it)>;
+        { T::decode(input_it) } noexcept -> std::same_as<code_point>;
+        { T::validate(input_it, end_it) } noexcept -> std::same_as<error_code>;
+        { T::next_code_point(input_it) } noexcept -> std::same_as<decltype(input_it)>;
+        { T::prev_code_point(input_it) } noexcept -> std::same_as<decltype(input_it)>;
+    };
 
 } // namespace unicode
 } // namespace bigj
