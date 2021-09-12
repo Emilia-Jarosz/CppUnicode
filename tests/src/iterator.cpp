fd2ac976e15ae9748041e62852542a2872d36194
Emil-Jarosz <emil.bigj.jarosz@gmail.com>
2021-09-12 10:09:40 +0100

    Rewrite tests for forward iterator

--- tests/src/iterator.cpp
+++ tests/src/iterator.cpp
@@ -2,88 +2,68 @@
 
 #include <bigj/unicode/iterator.hpp>
 #include <bigj/unicode/encoding/utf8.hpp>
+#include <bigj/unicode/encoding/utf16.hpp>
 #include <bigj/unicode/encoding/utf32.hpp>
 
 #include <catch2/catch_test_macros.hpp>
-#include <catch2/generators/catch_generators_adapters.hpp>
-#include <catch2/generators/catch_generators_random.hpp>
+#include <catch2/catch_template_test_macros.hpp>
 
-#include <array>
+#include <compare>
+#include <iterator>
 
 using namespace bigj;
 
-using unicode::utf8;
-using iterator = unicode::iterator<utf8>;
+TEMPLATE_TEST_CASE("code point iterator", "[iterator][forward_iterator]",
+    unicode::utf8,
+    unicode::utf16<std::endian::native>,
+    unicode::utf32<std::endian::native>
+) {
+    using E = TestType;
+    using iterator = unicode::iterator<E>;
 
-TEST_CASE("Default iterator is equal to nullptr", "[iterator]") {
-    constexpr auto it = iterator {};
+    static_assert(std::forward_iterator<iterator>);
+    static_assert(std::three_way_comparable<iterator, std::strong_ordering>);
 
-    STATIC_REQUIRE(it.address() == nullptr);
-}
-
-TEST_CASE("Iterator properties", "[iterator]") {
     auto code_points = GENERATE(
-        take(1000, random_string<unicode::utf32<std::endian::native>>(2))
+        take(100, random_string<E>(2))
     );
 
-    auto data = std::array<utf8::code_unit, 8> {};
-    auto length_1 = utf8::encoded_size(code_points[0]);
+    auto first = code_points.data();
+    auto second = E::next_code_point(first);
+    auto end = E::next_code_point(second);
 
-    utf8::encode(code_points[0], data.data());
-    utf8::encode(code_points[1], data.data() + length_1);
-
-    auto it_1 = iterator {data.data()};
-    auto it_2 = iterator {data.data() + length_1};
-
-    SECTION("dereference") {
-        CHECK(*it_1 == code_points[0]);
-        REQUIRE(*it_2 == code_points[1]);
-    }
+    auto first_it = iterator {first};
+    auto second_it = iterator {second};
+    auto end_it = iterator {end};
 
     SECTION("address") {
-        CHECK(it_1.address() == data.data());
-        REQUIRE(it_2.address() == data.data() + length_1);
+        REQUIRE(first_it.address() == first);
+        REQUIRE(second_it.address() == second);
+        REQUIRE(end_it.address() == end);
     }
 
-    SECTION("comparison") {
-        CHECK(it_1 != it_2);
-        CHECK(it_1 < it_2);
-        REQUIRE(it_2 > it_1);
+    SECTION("dereference") {
+        REQUIRE(*first_it == E::decode(first));
+        REQUIRE(*second_it == E::decode(second));
     }
 
     SECTION("pre-increment") {
-        auto it_3 = it_1;
+        auto it = first_it;
 
-        CHECK(it_3 == it_1);
-        CHECK(++it_3 == it_2);
-        CHECK(it_3 == it_2);
-        REQUIRE(it_3.address() == data.data() + length_1);
+        REQUIRE(it == first_it);
+        REQUIRE(++it == second_it);
+        REQUIRE(it == second_it);
+        REQUIRE(++it == end_it);
+        REQUIRE(it == end_it);
     }
 
     SECTION("post-increment") {
-        auto it_3 = it_1;
+        auto it = first_it;
 
-        CHECK(it_3 == it_1);
-        CHECK(it_3++ == it_1);
-        CHECK(it_3 == it_2);
-        REQUIRE(it_3.address() == data.data() + length_1);
-    }
-
-    SECTION("pre-decrement") {
-        auto it_3 = it_2;
-
-        CHECK(it_3 == it_2);
-        CHECK(--it_3 == it_1);
-        CHECK(it_3 == it_1);
-        REQUIRE(it_3.address() == data.data());
-    }
-
-    SECTION("post-decrement") {
-        auto it_3 = it_2;
-
-        CHECK(it_3 == it_2);
-        CHECK(it_3-- == it_2);
-        CHECK(it_3 == it_1);
-        REQUIRE(it_3.address() == data.data());
+        REQUIRE(it == first_it);
+        REQUIRE(it++ == first_it);
+        REQUIRE(it == second_it);
+        REQUIRE(it++ == second_it);
+        REQUIRE(it == end_it);
     }
 }
